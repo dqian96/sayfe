6cd524c442825306e5e8a4ae234c8e5d98456025
dqian <davidqian1996@gmail.com>
2016-01-09 23:57:24 -0500

    Splash screen + Questions

--- app/src/main/java/ca/sayfe/sayfe/MainActivity.java
+++ app/src/main/java/ca/sayfe/sayfe/MainActivity.java
@@ -1,7 +1,10 @@
 package ca.sayfe.sayfe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //define button by connecting it to the resource by id
+        Button mainActivityFirstButton = (Button) findViewById(R.id.mainActivityFirstButton);
+        //adding a listener
+        mainActivityFirstButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, QuestionListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //define button by connecting it to the resource by id
+        Button mainActivitySecondButton = (Button) findViewById(R.id.mainActivitySecondButton);
+        //adding a listener
+        mainActivitySecondButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //define button by connecting it to the resource by id
+        Button mainActivityThirdButton = (Button) findViewById(R.id.mainActivityThirdButton);
+        //adding a listener
+        mainActivityThirdButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Intent intent = new Intent(MainActivity.this, SupportActivity.class);
+                //startActivity(intent);
+            }
+        });
+
+
     }
 }
