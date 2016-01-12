32657a40d8d5db6171a32bf2466f5476b8d6106d
dqian <davidqian1996@gmail.com>
2016-01-11 20:11:36 -0500

    Main Activity Frontend Changes

--- app/src/main/java/ca/sayfe/sayfe/MainActivity.java
+++ app/src/main/java/ca/sayfe/sayfe/MainActivity.java
@@ -13,31 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Button to go to Questions Activity
         //define button by connecting it to the resource by id
-        Button mainActivityFirstButton = (Button) findViewById(R.id.mainActivityFirstButton);
+        Button mainActivityQuestionsButton = (Button) findViewById(R.id.mainActivityQuestionsButton);
         //adding a listener
-        mainActivityFirstButton.setOnClickListener(new View.OnClickListener() {
+        mainActivityQuestionsButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, QuestionListActivity.class);
                 startActivity(intent);
             }
         });
 
-        //define button by connecting it to the resource by id
-        Button mainActivitySecondButton = (Button) findViewById(R.id.mainActivitySecondButton);
-        //adding a listener
-        mainActivitySecondButton.setOnClickListener(new View.OnClickListener() {
+        //Button to go to map/unsafe activity
+        Button mainActivityMapButton = (Button) findViewById(R.id.mainActivityMapButton);
+        mainActivityMapButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, MapActivity.class);
                 startActivity(intent);
             }
         });
 
-
-        //define button by connecting it to the resource by id
-        Button mainActivityThirdButton = (Button) findViewById(R.id.mainActivityThirdButton);
-        //adding a listener
-        mainActivityThirdButton.setOnClickListener(new View.OnClickListener() {
+        //Button to go to support activity
+        Button mainActivitySupportButton = (Button) findViewById(R.id.mainActivitySupportButton);
+        mainActivitySupportButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //Intent intent = new Intent(MainActivity.this, SupportActivity.class);
                 //startActivity(intent);
--- app/src/main/java/ca/sayfe/sayfe/MapActivity.java
+++ app/src/main/java/ca/sayfe/sayfe/MapActivity.java
@@ -2,36 +2,13 @@ package ca.sayfe.sayfe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.google.android.gms.location.places.ui.PlacePicker
+
 public class MapActivity extends AppCompatActivity {
-
-
-    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        mGoogleApiClient = new GoogleApiClient
-                .Builder(this)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .build();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    protected void onStop() {
-        mGoogleApiClient.disconnect();
-        super.onStop();
+        setContentView(R.layout.activity_map);
     }
 }
 
